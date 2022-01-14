b1bd0299da241b47e548f7733a0a5afed6ea987b
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-14 16:52:00 +0700

    add

--- go.mod
+++ go.mod
@@ -26,6 +26,7 @@ require (
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/matthewhartstonge/argon2 v0.1.5 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-shellwords v1.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
--- models/user.go
+++ models/user.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/matthewhartstonge/argon2"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	ID        uint   `gorm:"primaryKey;autoIncrement;not null"`
@@ -10,4 +15,15 @@ type User struct {
 	IsAdmin   bool   `gorm:"type:bool;default:false"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func (user *User) BeforeCreate(db *gorm.DB)error{
+	user.Password = hashPassword(user.Password)
+	return nil
+}
+
+func  hashPassword(password string) string {
+	argon := argon2.DefaultConfig()
+	encoded,_:= argon.HashEncoded([]byte(password))
+	return string(encoded)
 }
