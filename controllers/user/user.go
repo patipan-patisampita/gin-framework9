964f657c7e96c39971f4e64da1b939218a48096c
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-21 14:50:42 +0700

    add

--- controllers/user/user.go
+++ controllers/user/user.go
@@ -2,21 +2,25 @@ package usercontroller
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/patipan-patisampita/gin-framework9/configs"
 	"github.com/patipan-patisampita/gin-framework9/models"
 )
 
+// CRUD
+//Read
 func GetAll(c *gin.Context) {
 	var users []models.User
-	configs.DB.Find(&users)
+	configs.DB.Order("id desc").Find(&users)
 
+	//SQL
+	configs.DB.Raw("SELECT * FROM users ORDER by id desc").Scan(&users)
 	c.JSON(200, gin.H{
 		"data new": users,
 	})
 }
 
+//Create
 func Register(c *gin.Context) {
 	var input InputRegister
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -29,11 +33,17 @@ func Register(c *gin.Context) {
 		Email:    input.Email,
 		Password: input.Password,
 	}
+	//check repeat email
+	userExist := configs.DB.Where("email = ?", input.Email).First(&user)
+	if userExist.RowsAffected == 1 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "มีผู้ใช้งานอีเมล์นี้ในระบบแล้ว"})
+		return
+	}
 	result := configs.DB.Debug().Create(&user)
 
 	//db error
-	if result.Error !=nil{
-		c.JSON(http.StatusBadRequest,gin.H{"error":result.Error})
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error})
 		return
 	}
 
@@ -48,14 +58,32 @@ func Login(c *gin.Context) {
 	})
 }
 
+//Read Get by id
 func GetById(c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+	result := configs.DB.First(&user, id)
+
+	if result.RowsAffected < 1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "ไม่พบข้อมูล"})
+		return
+	}
+
 	c.JSON(200, gin.H{
-		"data": "id",
+		"data": user,
 	})
 }
 
 func SearchByFullName(c *gin.Context) {
 	fullname := c.Query("fullname")
+
+	var users[]models.User
+	result := configs.DB.Where("fullname LIKE ?","%" + fullname + "%").Find(&users)
+	if result.RowsAffected<1{
+		c.JSON(http.StatusNotFound, gin.H{"error": "ไม่พบข้อมูล"})
+		return
+	}
+
 	c.JSON(200, gin.H{
 		"data": fullname,
 	})
--- models/user.go
+++ models/user.go
@@ -8,22 +8,22 @@ import (
 )
 
 type User struct {
-	ID        uint   `gorm:"primaryKey;autoIncrement;not null"`
-	Fullname  string `gorm:"type:varchar(255);not null"`
-	Email     string `gorm:"type:varchar(255);not null;unique"`
-	Password  string `gorm:"type:varchar(255);not null"`
-	IsAdmin   bool   `gorm:"type:bool;default:false"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement;not null"`
+	Fullname  string    `json:"fullname" gorm:"type:varchar(255);not null"`
+	Email     string    `json:"email" gorm:"type:varchar(255);not null;unique"`
+	Password  string    `json:"_" gorm:"type:varchar(255);not null"`
+	IsAdmin   bool      `json:"is_admin" gorm:"type:bool;default:false"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
-func (user *User) BeforeCreate(db *gorm.DB)error{
+func (user *User) BeforeCreate(db *gorm.DB) error {
 	user.Password = hashPassword(user.Password)
 	return nil
 }
 
-func  hashPassword(password string) string {
+func hashPassword(password string) string {
 	argon := argon2.DefaultConfig()
-	encoded,_:= argon.HashEncoded([]byte(password))
+	encoded, _ := argon.HashEncoded([]byte(password))
 	return string(encoded)
 }
